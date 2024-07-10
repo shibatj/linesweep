03db096b940b7b0599b99f268fb68d7f104af6db
Phanuwat <115919092+shibatj@users.noreply.github.com>
2024-07-10 12:17:22 +0700

    .

--- linesweep.cpp
+++ linesweep.cpp
@@ -13,19 +13,21 @@ int main()
     {
         int a, b;
         scanf("%d%d", &a, &b);
-        event.push_back({a, 1}); // add person
-        event.push_back({b, 2}); // remove person
+        event.push_back({b, 1}); // remove person
+        event.push_back({a, 2}); // add person
     }
     sort(event.begin(), event.end());
 
     int cnt = 0, mx = 0;
     for (auto e : event){
-        if(e.second == 1) { // add pseson
+        if(e.second == 1) { // add remove
             ++cnt;
             mx = max(mx, cnt);
-        }else{ // remove person
+        }else{ // add person
             --cnt;
+            mx = max(mx, cnt);
         }
+        printf("%d\n", mx);
     }
     return 0;
 }
